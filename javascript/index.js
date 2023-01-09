c75176a02f13cb631e2c27bd3d1de82e188ff849
kaowsoy <kaowsoypoemsomboon@gmail.com>
2023-01-09 12:01:10 +0100

    nye ændringer

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,20 @@
+const photos = Array.from(document.querySelectorAll('#photo'));
+
+const scalingImage = (currentPhoto => {
+  currentPhoto.classList.add("scaleImage");
+  
+  setTimeout(() => {
+    currentPhoto.classList.remove('scaleImage');
+  }, 4500);
+})
+
+photos.map((photo) => {
+  photo.addEventListener('click', Event => {
+    scalingImage(Event.target);
+  })
+});
+
+
 let timeinterval = setInterval(nextimages, 4000);
 
 const buttons = document.querySelectorAll("[data-carousel-button]");
